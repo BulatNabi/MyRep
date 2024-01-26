cca9e8f03fab66bc0dca1b4a136570b3217f6903
BulatNabi <nabiullin.bulat007@gmail.com>
2024-01-26 18:08:20 +0300

    first commit

---
+++ file1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Console = System.Console;
+
+namespace file1 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HI");          
+        }
+    }
+}
---
+++ file1/Program1.cs
@@ -0,0 +1,9 @@
+﻿namespace file1;
+
+public class Program1
+{
+    static int HiHi()
+    {
+        return 100;
+    }
+}
